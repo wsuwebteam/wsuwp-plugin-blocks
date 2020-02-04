10b63082e50c065fb77853306913a86adb0feab4
Nic Ford <nicdford@gmail.com>
2020-02-03 16:56:27 -0800

    Restructure imports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * Gutenberg Blocks
+ *
+ */
+
+// Filters
+// import './columns/index.js';
+
+// Blocks
+// import './buttons/index.js'; TODO: create button
+import './paragraph/index.js'
+import './test-block/index.js';
