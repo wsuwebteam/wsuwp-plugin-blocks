145699cb86fecc1c076e56407da0f65c2b07333e
Nic Ford <nicdford@gmail.com>
2020-02-06 10:34:11 -0800

    Final approved interface for creating buttons

--- src/button/index.js
+++ src/button/index.js
@@ -6,10 +6,15 @@
  */
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { URLInputButton } = wp.editor;
 const { RichText } = wp.blockEditor;
 const { Path, SVG } = wp.components;
+const { withState } = wp.compose;
 
+/**
+ *
+ * Local Dependencies
+ *
+ */
 const buttonIcon = (
 	<SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 		<Path d="M19 6H5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 10H5V8h14v8z" />
@@ -24,14 +29,9 @@ registerBlockType('wsuwp-plugin-blocks/button', {
 	attributes: {
 		buttonContent: {
 			type: 'string',
-			source: 'text'
-		},
-		buttonUrl: {
-			type: 'string',
-			source: 'attribute',
-			selector: '.wsu-c-button',
-			attribute: 'href'
-		},
+			source: 'html',
+			selector: '.wsu-c-button'
+		}
 	},
 	supports: {
 		className: false
@@ -56,35 +56,27 @@ registerBlockType('wsuwp-plugin-blocks/button', {
 			setAttributes({ buttonContent: newButtonContent });
 		};
 
-		const onChangeButtonUrl = (newButtonUrl) => {
-			setAttributes({ buttonUrl: newButtonUrl });
-		}
-
 		return (
 			<>
-				<a href={buttonUrl} className="wsu-c-button">
+				<div className="wsu-c-button">
 					<RichText
 						placeholder={__('Add Button Label Text...', 'wsuwp-plugin-blocks')}
 						value={buttonContent}
 						onChange={onChangeButtonContent}
-						allowedFormats={[]}
+						allowedFormats={['core/link']}
 						multiline={'false'}
 					/>
-				</a>
-				<URLInputButton
-					className="testing-input"
-					label={__('Button URL Input', 'wsuwp-plugin-blocks')}
-					onChange={onChangeButtonUrl}
-					url={buttonUrl}
-				/>
+				</div>
 			</>
 		);
 	},
 	save: (props) => {
 		return (
-			<a href={props.attributes.buttonUrl} className="wsu-c-button">
-				<RichText.Content value={props.attributes.buttonContent} />
-			</a>
+			<>
+				<div className="wsu-c-button">
+					<RichText.Content value={props.attributes.buttonContent} />
+				</div>
+			</>
 		);
 	},
 });
