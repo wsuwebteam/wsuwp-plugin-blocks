2f8ef6132ae29787a360ce68b1ada674b58a2d63
Nic Ford <nicdford@gmail.com>
2020-05-06 15:20:14 -0700

    Add form input labels

--- blocks/src/banner/editor.js
+++ blocks/src/banner/editor.js
@@ -1,71 +1,84 @@
 const { __ } = wp.i18n;
 const { MediaPlaceholder } = wp.editor;
-const { RichText } = wp.editor;
+const { PlainText } = wp.editor;
 const { URLInput } = wp.editor;
+const { InspectorControls } = wp.editor;
 
+const editor = wp.editor;
 
-const edit = ( { className, attributes, setAttributes } ) => {
+console.log(editor);
+
+const edit = ({ className, attributes, setAttributes }) => {
 
 	console.info(attributes);
 
 	return (
-		<div className="wsu-c-block__banner__wrapper">
-			<div className="wsu-c-hero__wrapper">
-				<div className="wsu-c-hero__container" style={{backgroundImage:"url(" + attributes.img_src + ")"}}>
-					<div className="wsu-c-hero__content">
-						<div className="wsu-c-hero__title">{ attributes.title }</div>
-						<div className="wsu-c-hero__subtitle">{ attributes.subtitle }</div>
-						<a href="#" className="wsu-c-hero__button">{ attributes.button_text }</a>
+		<div>
+			<div className="wsu-c-block__banner__wrapper">
+				<div className="wsu-c-hero__wrapper">
+					<div className="wsu-c-hero__container" style={{ backgroundImage: "url(" + attributes.img_src + ")" }}>
+						<div className="wsu-c-hero__content">
+							<div className="wsu-c-hero__title">{attributes.title}</div>
+							<div className="wsu-c-hero__subtitle">{attributes.subtitle}</div>
+							<div class="wsu-c-button wsu-button-sm">
+								<a href="#" class="wsu-c-button_link">{attributes.button_text}</a>
+							</div>
+						</div>
+						<p className="wsu-c-hero__image-caption">By consectetur adipiscing elit donec fringilla</p>
 					</div>
-					<p className="wsu-c-hero__image-caption">By consectetur adipiscing elit donec fringilla</p>
 				</div>
-			</div>
-			<div className="wsu-c-block__banner__editor__wrapper">
-				<div className="wsu-c-block__banner__editor__media">
-					<MediaPlaceholder
-						onSelect = {
-							( el ) => {
-								setAttributes( { img_src: el.url } );
+				<div className="wsu-c-block__banner__editor__wrapper">
+					<div className="wsu-c-block__banner__editor__media">
+						<MediaPlaceholder
+							onSelect={
+								(el) => {
+									setAttributes({ img_src: el.url });
+								}
 							}
-						}
-						allowedTypes = { [ 'image' ] }
-						multiple = { false }
-						labels = { { title: 'Banner Image' } }
-					>
-					</MediaPlaceholder>
-				</div>
-				<div className="wsu-c-block__banner__editor__content">
-					<RichText
-						tagName="div"
-						className={ 'wsu-c-block__banner__editor__content__title' }
-						value={ attributes.title }
-						onChange={ ( title ) => setAttributes( { title } ) }
-						allowedFormats={[]}
-						placeholder={'Banner Title Text'}
-					/>
-					<RichText
-						tagName="div"
-						className={ 'wsu-c-block__banner__editor__content__subtitle' }
-						value={ attributes.subtitle }
-						onChange={ ( subtitle ) => setAttributes( { subtitle } ) }
-						allowedFormats={[]}
-						placeholder={'Banner Subtitle Text'}
-					/>
-					<RichText
-						tagName="div"
-						className={ 'wsu-c-block__banner__editor__content__button-text' }
-						value={ attributes.button_text }
-						onChange={ ( button_text ) => setAttributes( { button_text } ) }
-						allowedFormats={[]}
-						placeholder={'Button Text'}
-						multiline={'false'}
-					/>
-					<URLInput
-						className={ 'wsu-c-block__banner__editor__content__link' }
-						value={ attributes.url }
-						onChange={ ( url, post ) => setAttributes( { url } ) }
-						placeholder={'Banner Link'}
-					/>
+							allowedTypes={['image']}
+							multiple={false}
+							labels={{ title: 'Banner Image' }}
+						>
+						</MediaPlaceholder>
+					</div>
+					<div className="components-base-control__field">
+						<label htmlFor="bannerTitleText">Title Text</label>
+						<PlainText
+							id="bannerTitleText"
+							className={'components-text-control__input'}
+							value={attributes.title}
+							onChange={(title) => setAttributes({ title })}
+							placeholder={'Title Text'}
+						/>
+						<label htmlFor="bannerSubtitleText">Subtitle Text</label>
+						<PlainText
+							id="bannerSubtitleText"
+							className={'components-text-control__input'}
+							value={attributes.subtitle}
+							onChange={(subtitle) => setAttributes({ subtitle })}
+							placeholder={'Subtitle Text'}
+						/>
+						<div className="wsu-c-form__field-container--textarea"></div>
+						<label htmlFor="bannerButtonText">Button Text</label>
+						<PlainText
+							id="bannerButtonText"
+							className={'components-text-control__input'}
+							value={attributes.button_text}
+							onChange={(button_text) => setAttributes({ button_text })}
+							placeholder={'Button Text'}
+							multiline={'false'}
+						/>
+
+
+						<label htmlFor="bannerButtonLink">Button Link</label>
+						<URLInput
+							id="bannerButtonLink"
+							className={'wsu-c-block__banner__editor__content__link'}
+							value={attributes.url}
+							onChange={(url, post) => setAttributes({ url })}
+							placeholder={'Button Link'}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
