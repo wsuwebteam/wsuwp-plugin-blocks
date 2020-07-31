4cd461c02fe5f9d0b717436f37c0e4fc8f743966
Danial Bleile <danbleile@gmail.com>
2020-07-31 02:40:48 -0700

    Add Callout Block

--- blocks/src/index.js
+++ blocks/src/index.js
@@ -9,22 +9,13 @@
 
 // Blocks
 
-//import './button/index.js';
-//import './banner/index.js';
+
 import './post-title/index.js';
-//import './heading/index.js';
-//import './paragraph/index.js';
-//import './test-block/index.js';
+
 import './editor.scss';
 import './columns/editor.scss';
 
-//import './columns-single/index.js';
-//import './columns-halves/index.js';
-//import './columns-thirds/index.js';
-//import './columns-quarters/index.js';
-//import './columns-sidebar-left/index.js';
-//import './columns-sidebar-right/index.js';
-//import './column/index.js';
+
 import './search-bar/index.js'; 
 
 import './freeform/index.js';
@@ -38,3 +29,4 @@ import '../content-heading/editor/block';
 import '../content-button/editor/block';
 import '../content-cards/editor/block';
 import '../content-card/editor/block';
+import '../content-callout/editor/block';
