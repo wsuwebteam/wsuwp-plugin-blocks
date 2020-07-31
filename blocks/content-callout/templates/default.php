2e7f925a5b7e984a07d5d01421a6114d8709c9c8
Danial Bleile <danbleile@gmail.com>
2020-07-31 03:21:54 -0700

    Fix callout template

--- blocks/content-callout/templates/default.php
+++ blocks/content-callout/templates/default.php
@@ -3,30 +3,32 @@
 		<?php if ( ! empty( $atts['image_src'] ) ) : ?>
 			<div class="wsu-c-callout__image-frame">
 				<div class="wsu-c-callout__image-wrapper">
-				<a href="#" class="wsu-c-callout__image-link"
-					><img
-					class="wsu-c-callout__image"
-					src="<?php echo esc_url( $atts['image_src'] ); ?>"
-					alt="Alt Text"
-				/></a>
+					<?php if ( ! empty( $atts['link'] ) ) : ?><a href="<?php echo esc_url( $atts['link'] ); ?>" class="wsu-c-callout__image-link"><?php endif; ?>
+					<img
+						class="wsu-c-callout__image"
+						src="<?php echo esc_url( $atts['image_src'] ); ?>"
+						alt="<?php echo esc_url( $atts['image_alt'] ); ?>""
+						/>
+					<?php if ( ! empty( $atts['link'] ) ) : ?></a><?php endif; ?>
 				</div>
 			</div>
 		<?php endif; ?>
 	  	<div class="wsu-c-callout__content">
-		<h3 class="wsu-c-callout__title">
-		  <a href="#" class="wsu-c-callout__title-link"
-			>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a
-		  >
-		</h3>
-		<div class="wsu-c-callout__description">
-			<?php echo $content; ?>
-		</div>
-		<div class="wsu-c-button__wrapper">
-		  <a
-			class="wsu-c-button--white wsu-c-button--small wsu-c-button--round wsu-c-button"
-			>Learn More</a
-		  >
-		</div>
-	  </div>
+			<h3 class="wsu-c-callout__title">
+				<?php if ( ! empty( $atts['link'] ) ) : ?><a href="<?php echo esc_url( $atts['link'] ); ?>" class="wsu-c-callout__title-link"><?php endif; ?>
+				<?php echo esc_html( $atts['title'] ); ?>
+				<?php if ( ! empty( $atts['link'] ) ) : ?></a><?php endif; ?>
+			</h3>
+			<div class="wsu-c-callout__description">
+				<?php echo $content; ?>
+			</div>
+			<?php if ( ! empty( $atts['button_text'] ) ) : ?>
+				<div class="wsu-c-button__wrapper">
+					<a class="wsu-c-button--white wsu-c-button--small wsu-c-button--round wsu-c-button" href="<?php echo esc_url( $atts['link'] ); ?>" >
+					<?php echo esc_html( $atts['button_text'] ); ?>
+					</a >
+				</div>
+			<?php endif; ?>
+	  	</div>
 	</div>
-  </div>
+</div>
--- includes/include-plugin.php
+++ includes/include-plugin.php
@@ -8,7 +8,7 @@
  */
 class Plugin {
 
-	protected static $version = '0.5.0';
+	protected static $version = '0.5.1';
 
 	protected static $options;
 
