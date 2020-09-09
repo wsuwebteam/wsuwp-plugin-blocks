afa7098f2c40e96e484972781333900d3cdbd668
Danial Bleile <danbleile@gmail.com>
2020-09-09 08:50:46 -0700

    Merge

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '8aea1584167b2bce748fd6f417b743ec');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '6f4899e2c713afebdd26c66fad937f3c');
