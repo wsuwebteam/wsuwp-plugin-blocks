703bee54d2e75d99f9e7e84ca15b85e4bf8406c8
Nic Ford <nicdford@gmail.com>
2020-09-09 16:49:26 -0700

    Update column icon

--- packages/blocks/content-column/editor/block.js
+++ packages/blocks/content-column/editor/block.js
@@ -23,6 +23,7 @@ registerBlockType(
 		title: "WSU Column",
 		parent: [ 'core/columns-single' ],
 		category: "common",
+		icon: <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M41 14C41 12.8954 40.1046 12 39 12H9C7.89543 12 7 12.8954 7 14V34C7 35.1046 7.89543 36 9 36H39C40.1046 36 41 35.1046 41 34V14ZM39 34V14H9V34H39Z" /></svg>,
 		attributes: {
 			...globalBlockAtts,
 			format: {
