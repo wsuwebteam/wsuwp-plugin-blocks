39462e2fb84734d04483f65410d2eb4797554a99
Danial Bleile <danbleile@gmail.com>
2020-09-29 11:02:34 -0700

    Enhancement | Move plugin scripts into a specific class

--- includes/include-plugin.php
+++ includes/include-plugin.php
@@ -55,6 +55,7 @@ class Plugin {
 		require_once __DIR__ . '/include-options.php';
 		require_once __DIR__ . '/include-blocks.php';
 		require_once __DIR__ . '/include-customizer.php';
+		require_once __DIR__ . '/include-plugin-scripts.php';
 
 	}
 
