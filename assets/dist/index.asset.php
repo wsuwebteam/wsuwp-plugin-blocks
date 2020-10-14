349650805b5d459c513d2309894a0cd4b6d7e9fe
Nic Ford <nicdford@gmail.com>
2020-10-14 13:35:53 -0700

    Auto stash before merge of "enhancement/em-admitted-student-landing-page" and "1.x"

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '577a29c30955980c74966e526e284911');
+=======
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'cb1c249aac857b7e1dfd71c37077a124');
+>>>>>>> Stashed changes
