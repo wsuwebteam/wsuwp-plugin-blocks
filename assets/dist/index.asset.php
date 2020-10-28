f7b671357ef50d47707142eb85b25e4a79b0a481
Danial Bleile <danbleile@gmail.com>
2020-10-28 15:02:58 -0700

    Bump Version 0.23.1

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '953d3b2a39bf9913913fa28770ad8207');
-=======
 <?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'a848cd4eb2ac22282faa09349c611abe');
->>>>>>> 1.x
