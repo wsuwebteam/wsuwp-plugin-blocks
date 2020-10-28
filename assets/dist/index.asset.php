92cee0dc674c6a821b09144aa4718551afb71ae4
Nic Ford <nicdford@gmail.com>
2020-10-28 09:54:37 -0700

    Bump version

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '82032f1a2e9241120a6735d630b19623');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '42ec2630b1a0f33ed74093c36e3f7f63');
