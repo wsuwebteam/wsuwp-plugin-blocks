227654bdba7b9bc46362a8d810a879340b11a3bc
Danial Bleile <danbleile@gmail.com>
2020-10-29 11:12:42 -0700

    Hotfix | bad logic

--- packages/blocks/content-group/templates/default.php
+++ packages/blocks/content-group/templates/default.php
@@ -5,7 +5,7 @@
 	<?php if ( ! empty( $atts['inline_style'] ) ) : ?> style="<?php echo esc_attr( $atts['inline_style'] ); ?>"<?php endif; ?>
 	>
 	<?php echo EM_Content_Decorator::render_from_array( $atts['decorators'] ); ?>
-	<?php if ( empty( $atts['limit_content_width'] ) ) : ?><div class="wsu-c-group__content wsu-c-container" ><?php endif; ?>
+	<?php if ( ! empty( $atts['limit_content_width'] ) ) : ?><div class="wsu-c-group__content wsu-c-container" ><?php endif; ?>
 		<?php echo $content; ?>
-	<?php if ( empty( $atts['limit_content_width'] ) ) : ?></div><?php endif; ?>
+	<?php if ( ! empty( $atts['limit_content_width'] ) ) : ?></div><?php endif; ?>
 </div>
